3e8f001021abb143725936402edf779f64b75a60
Julien M'poy <Julien M'poy>
2016-05-31 20:56:28 +0200

    Ajout du module permettant d'appeler l'API slack. Modification du bot de manière à ce qu'il ne réponde pas à ses propres messages. La solution n'est pas la meilleures mais fonctionne pour l'instant.

--- zerabot/testbot.py
+++ zerabot/testbot.py
@@ -12,12 +12,51 @@ from api import api_call
 from bot_token import TOKEN
 
 
+async def check_user(user):
+    """ returns 0: online, 1: offline, 2: not found, 3: error """
+    async with ClientSession() as session:
+        url = 'https://api.twitch.tv/kraken/streams/{}'.format(user)
+        async with session.get(url) as response:
+            try:
+                info = await response.read()
+                info = json.loads(info.decode('utf-8'))
+                # print(info)
+                try:
+                    if info['stream'] == None:
+                        status = 1
+                        return "{0} is offline".format(user)
+                    else:
+                        status = 0
+                        return "{0} is playing {1} in front of {2} viewers, watch here {3}".format(info['stream']['channel']['display_name'], info['stream']['game'], info['stream']['viewers'], info['stream']['channel']['url'])
+                except KeyError as e:
+                    return "{0} doesn't exist.".format(user)
+            except URLError as e:
+                    pass
+                    if e.reason == 'Not Found' or e.reason == 'Unprocessable Entity':
+                        status = 2
+                        return "{0} does not exist".format(user)
+                    else:
+                        status = 3
+                        return "an error occured"
+
+async def answer(user_token, message):
+    data = {"token": TOKEN, "channel": user_token,"text": message}
+    await api_call("chat.postMessage", data, TOKEN)
+
+
 async def consumer(message):
     """Display the message."""
-    if message.get('type') == 'message':
-        user = await api_call('users.info',{'user': message.get('user')})
+    #print("message user:", message.get('user'))
+    message_user = message.get('user')
+    if message.get('type') == 'message' and message_user != None:
+        #user = await api_call('users.info',{'user': message.get('user')})
         user_status = await check_user(message["text"])
-        print("{0}: {1}".format(user["user"]["name"],user_status))
+        #print("{0}: {1}".format(user["user"]["name"],user_status))
+        #bot_answer = "{0}: {1}".format(user["user"]["name"],user_status)
+        bot_answer = "{0}".format(user_status)
+        #print(bot_answer)
+        #print(message)
+        await answer(message['channel'], bot_answer)
     else:
         print(message, file=sys.stderr)
 
@@ -32,6 +71,10 @@ async def bot(token=TOKEN):
             async for msg in ws:
                 assert msg.tp == aiohttp.MsgType.text
                 message = json.loads(msg.data)
+                #print(message)
+                #my_self = rtm['self']['id']
+                #print(my_self)
+                #print(rtm['self']['name'])
                 asyncio.ensure_future(consumer(message))
 
 
@@ -39,29 +82,3 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(bot())
     loop.close()
-
-
-async def check_user(user):
-    """ returns 0: online, 1: offline, 2: not found, 3: error """
-    async with ClientSession() as session:
-        url = 'https://api.twitch.tv/kraken/streams/{}'.format(user)
-        async with session.get(url) as response:
-            try:
-                info = await response.read()
-                info = json.loads(info.decode('utf-8'))
-                # print(info)
-                if info['stream'] == None:
-                    status = 1
-                    return "{0}, is offline".format(user)
-                else:
-                    status = 0
-                    print(info['stream']['channel']['display_name'], "is playing", info['stream']['game'], "in front of", info['stream']['viewers'], "viewers")
-                    return "Watch here {0}".format(info['stream']['channel']['url'])
-            except URLError as e:
-                    pass
-                    if e.reason == 'Not Found' or e.reason == 'Unprocessable Entity':
-                        status = 2
-                        return "{0} does not exist".format(user)
-                    else:
-                        status = 3
-                        return "an error occured"

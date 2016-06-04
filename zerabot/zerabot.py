b3ea6615ada09db822588523401c6df29bbb58d1
Julien M'poy <Julien M'poy>
2016-06-04 09:23:52 +0200

    Plus de précision dans le message d'aide. Gestion des arguments manquants pour la première commande. Documentation des nouvelles fonctions et actualisation des fonctions plus anciennes.

--- zerabot/zerabot.py
+++ zerabot/zerabot.py
@@ -11,41 +11,52 @@ from bot_token import TOKEN
 from twitch import check_user
 
 bot_infos = dict()
+handled_commands = ("streams","help")
+bot_help = """
+Hmm... You seem to have asked for help or tried to do something wrong with me. Call me with `@{name}: command`
+*Avalaible commands*:
+    •`@{name}: streams toto` checks if toto is streaming.
+            """
+async def send_help(user):
+    """ Makes the bot send help text to the specified user. """
+    global bot_help
+    bot_answer = bot_help.format(name=bot_infos['name'])
+    await answer(user, bot_answer)
 
-async def answer(user_token, message):
-    data = {"token": TOKEN, "channel": user_token,"text": message}
+
+async def answer(user, message):
+    """ Sends a message to the user. """
+    data = {"token": TOKEN, "channel": user,"text": message}
     await api_call("chat.postMessage", data, TOKEN)
 
 
 async def consumer(message):
-    """Display the message."""
-    global bot_infos
-    #print("message user:", message.get('user'))
+    """
+    Consumes the given message, parse it and makes the bot react
+    depending on the message.
+    """
+    global bot_infos, bot_help, handled_commands
     message_user = message.get('user')
-    print("received message:", message)
-    print(message_user, " != ", bot_infos['id'], message_user != bot_infos['id'])
     if message.get('type') == 'message' and message_user not in (bot_infos['id'], None):
-        #user = await api_call('users.info',{'user': message.get('user')})
-        # asked_user = re.search(r'(\S+)', message["text"].strip())
         text = message["text"].split(' ')
-        if text[0] != ("<@" + str(bot_infos['id']) + ">:"):
-            await answer(message['channel'], "Please call me")
+        if text[0] != ("<@" + str(bot_infos['id']) + ">:") or len(text) < 3:
+            await send_help(message['channel'])
 
         else:
-            asked_user = text[1]
-            user_status = await check_user(asked_user)
-        #print("{0}: {1}".format(user["user"]["name"],user_status))
-        #bot_answer = "{0}: {1}".format(user["user"]["name"],user_status)
-            bot_answer = "{0}".format(user_status)
-        #print(bot_answer)
-        #print(message)
-            await answer(message['channel'], bot_answer)
-    #else:
-    #    print(message, file=sys.stderr)
+            if text[1] in handled_commands:
+                if text[1] == handled_commands[0]:
+                    asked_user = text[2]
+                    user_status = await check_user(asked_user)
+                    bot_answer = "{0}".format(user_status)
+                    await answer(message['channel'], bot_answer)
+            else:
+                await send_help(message['channel'])
+    else:
+       print(message, file=sys.stderr)
 
 
 async def bot(token=TOKEN):
-    """Create a bot that joins Slack."""
+    """Creates a Slack bot with whitch to communicate."""
     rtm = await api_call("rtm.start")
     assert rtm['ok'], "Error connecting to RTM."
 
@@ -54,13 +65,9 @@ async def bot(token=TOKEN):
             async for msg in ws:
                 assert msg.tp == aiohttp.MsgType.text
                 message = json.loads(msg.data)
-                #print(message)
-                # infos = {'id': rtm['self']['id']}
                 global bot_infos
                 bot_infos.update({'id': rtm['self']['id'], 'name': rtm['self']['name']})
                 print(bot_infos)
-                #print(my_self)
-                #print(rtm['self']['name'])
                 asyncio.ensure_future(consumer(message))
 
 
